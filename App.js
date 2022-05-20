9f23a9329342c1c488c4a6a3837c04f41c577197
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-05-20 07:47:28 +0500

    initialize project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text, View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
